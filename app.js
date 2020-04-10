03a9a97046ba0bd240d0b0565dfbaabf2bccd0a9
raresmigea <raresini@yahoo.com>
2020-04-10 16:42:56 +0300

    updated app and chat

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+
 
 //set the template engine ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 //middlewares
-app.use(express.static('public'));
+app.use(express.static('public'))
+
 
 //routes
 app.get('/', (req, res) => {
-  res.render('index');
-});
+	res.render('index')
+})
 
-//listen on port
-server = app.listen(3000);
+//Listen on port 3000
+server = app.listen(3000)
 
-//socket instantiation
-const io = require('socket.io')(server);
 
+
+//socket.io instantiation
+const io = require("socket.io")(server)
 
 
 //listen on every connection
 io.on('connection', (socket) => {
-  console.log('new user connected!');
+	console.log('New user connected')
 
-/*
-when a new user is connecting to the app - default username: anonymous
-to do that: add a key to the socket
-the socker - represents each client connected to the socket
-*/
-  //default username
-  socket.username = "anonymous";
+	//default username
+	socket.username = "Anonymous"
 
-  //list on change_username
-  socket.on('change_username', (data) => {
-    socket.username = data.username;
-  });
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
 
-  //listen on new_messages
-  socket.on('new_message0', (data) => {
-    //broadcast the new message
-    io.sockets.emit('new_message', { message: data.message, username: socket.username });
-  });
-});
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
--- public/chat.js
+++ public/chat.js
@@ -1,33 +1,39 @@
 $(function(){
-  /*
-  On the client side: the goal is the opposite:
-  - each time the button change_username is clicked - the client will
-  send an event with the new value
-  */
-  //make connection
-  var socket = io.connect('http://localhost:3000');
+   	//make connection
+	var socket = io.connect('http://localhost:3000')
 
-  //buttons and inputs
-  var message = $("#message");
-  var username = $("#username");
-  var send_message = $("#send_message");
-  var send_username = $("#send_username");
-  var chatroom = $("#chatroom");
+	//buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+	var feedback = $("#feedback")
 
-  //emit a messsage
-  send_message.click(function(){
-    socket.emit('new_message', { message: message.val()});
-  });
+	//Emit message
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
 
-  //listen on new_message
-  socket.on('new_message', (data) => {
-    console.log(data);
-    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
-  });
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		feedback.html('');
+		message.val('');
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	})
 
-  //emit an username
-  send_username.click(function() {
-    console.log(username.val());
-    socket.emit('change_username', { username: username.val() });
-  });
+	//Emit a username
+	send_username.click(function(){
+		socket.emit('change_username', {username : username.val()})
+	})
+
+	//Emit typing
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	})
+
+	//Listen on typing
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	})
 });
