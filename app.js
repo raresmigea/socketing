3a2e7b47fd9ce8d22dd1093e5fb19f90dbdf0ada
raresmigea <raresini@yahoo.com>
2020-04-10 16:13:30 +0300

    added ejs and chat files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+//listen on port
+server = app.listen(3000);
+
 //socket instantiation
-const socket = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 //listen on every connection
 io.on('connection', (socket) => {
@@ -17,8 +20,5 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('index');
 });
-
-//listen on port
-server = app.listen(3000);
---
+++ public/chat.js
@@ -0,0 +1,4 @@
+$(function(){
+  //make connection
+  var socket = io.connect('http://localhost:3000');
+});
