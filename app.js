05985ecdad750dffa5d23d658d9f18567f5de1ae
raresmigea <raresini@yahoo.com>
2020-04-10 16:36:59 +0300

    refactoring app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 
+//set the template engine ejs
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 //listen on port
 server = app.listen(3000);
 
 //socket instantiation
 const io = require('socket.io')(server);
+
+
 
 //listen on every connection
 io.on('connection', (socket) => {
@@ -29,15 +42,4 @@ the socker - represents each client connected to the socket
     //broadcast the new message
     io.sockets.emit('new_message', { message: data.message, username: socket.username });
   });
-});
-
-//set the template engine ejs
-app.set('view engine', 'ejs');
-
-//middlewares
-app.use(express.static('public'));
-
-//routes
-app.get('/', (req, res) => {
-  res.render('index');
 });
