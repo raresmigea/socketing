e8f5035eceb3afdefb463c9245103430dee899f1
raresmigea <raresini@yahoo.com>
2020-06-06 23:49:34 +0300

    converted to ES6 modules

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import express, { static } from 'express';
 const app = express();
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
 
 //middlewares
-app.use(express.static('public'));
+app.use(static('public'));
 
 //routes
 app.get('/', (req, res) => {
