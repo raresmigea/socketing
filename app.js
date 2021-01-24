5eec53d3e1dfa379b4369fd7a1437f8c6e9a8565
raresmigea <raresini@yahoo.com>
2021-01-24 23:46:11 +0200

    updated comments

--- app.js
+++ app.js
@@ -22,24 +22,24 @@ const io = require('socket.io')(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  //default username
+  // default username
   socket.username = 'Anonymous';
 
-  //listen on change_username
+  // listen on change_username
   socket.on('change_username', (data) => {
     socket.username = data.username;
   });
 
   //listen on new_message
   socket.on('new_message', (data) => {
-    //broadcast the new message
+    // broadcast the new message
     io.sockets.emit('new_message', {
       message: data.message,
       username: socket.username,
     });
   });
 
-  //listen on typing
+  // listen on typing
   socket.on('typing', () => {
     socket.broadcast.emit('typing', { username: socket.username });
   });
